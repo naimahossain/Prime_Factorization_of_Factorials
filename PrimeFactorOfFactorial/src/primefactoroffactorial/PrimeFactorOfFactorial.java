bb2e564d6440fb6465b0178b0300206f69f2ab79
naima hossain <naima.academic@gmail.com>
2015-05-10 13:36:51 -0400

    IsPrime modified, print all prime factors(1-100)
    1. For prime number at least one divisor will be less than or equal to
    the root value. Is prime method modified to run till equal to the root
    value.
    
    2. a _primeArray is initiated to store all prime numbers between 1-100.
    Divide input numbers only by prime numbers to get prime factors.

--- PrimeFactorOfFactorial/src/primefactoroffactorial/PrimeFactorOfFactorial.java
+++ PrimeFactorOfFactorial/src/primefactoroffactorial/PrimeFactorOfFactorial.java
@@ -5,6 +5,7 @@
  */
 package primefactoroffactorial;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -12,6 +13,48 @@ import java.util.Scanner;
  * @author Naima
  */
 public class PrimeFactorOfFactorial {
+    ArrayList<Integer> _primeArray = new ArrayList<>();
+    
+     void Print(ArrayList<Integer> primeFactors){
+        primeFactors.stream().forEach((factor)->{
+            System.out.print(factor+", ");
+        });
+        System.out.println();
+    }
+     
+    void Init(){
+          InitPrimeFactorList();
+    }
+    
+    void InitPrimeFactorList (){ 
+        for(int i = 0; i < 100; i++){
+            if(IsPrime(i))
+                _primeArray.add(i);
+        }
+    }
+    
+    ArrayList<Integer> GetPrimeFactors(int n){
+        ArrayList<Integer> primeFactors = new ArrayList<>(); 
+        if(IsPrime(n) || n == 1){
+            primeFactors.add(n);
+            if(n != 1)
+                primeFactors.add(1);
+            return primeFactors;
+        }
+        int newN = n;
+        while(!IsPrime(newN) && (newN > 1)){
+            for (Integer num : _primeArray) {
+                if(newN % num == 0){
+                    primeFactors.add(num);
+                    newN /= num;
+                    break;
+                }
+            }
+        }
+        if(IsPrime(newN))
+            primeFactors.add(newN);
+        return primeFactors;
+    }
     
     boolean IsPrime(int n){
         if(n <= 3)
@@ -19,7 +62,7 @@ public class PrimeFactorOfFactorial {
         if(n % 2 == 0 || n % 3 == 0)
             return false;
         int root = (int)Math.floor(Math.sqrt(n));
-        for(int i = 5; i < root; i += 6){
+        for(int i = 5; i <= root; i += 6){
             if(n % i == 0 || n % (i+2) == 0)
                 return false;
         }
@@ -36,10 +79,14 @@ public class PrimeFactorOfFactorial {
      */
     public static void main(String[] args) {
         PrimeFactorOfFactorial pf = new PrimeFactorOfFactorial();
-        System.out.println("Enter a number between 1-100 to see if it's prime or not");
+        pf.Init();
+        System.out.println("Enter a number between 1-100 to see it's prime factors");
+        System.out.println("Enter out of range number to exit");
         int n = pf.Input();
-        String str = pf.IsPrime(n)? "Prime" : "Not Prime";
-        System.out.println(str);
+        while(n >= 1 && n <= 100){
+            pf.Print(pf.GetPrimeFactors(n));
+            n = pf.Input();
+        }
     }
     
 }
