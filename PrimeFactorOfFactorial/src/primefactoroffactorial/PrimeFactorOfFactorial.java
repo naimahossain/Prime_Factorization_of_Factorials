ee18016014b94f637e9ba97c72d1e0e9b5ed4931
naima hossain <naima.academic@gmail.com>
2015-05-10 12:35:49 -0400

    Finds if a number is prime or not
    1.  Finds if a number is prime or not :
    implemented IsPrime method according to the following discussion:
    all prime number are of the form (6k+1) or (6k-1), excluding 2 and 3.
    * Because, (6k+0)%3=0, (6k+2)%2=0, (6k+3)%3=0, (6k+4)%2=0.
    * Again, if a number is not a prime number there must be
    * at least one divisor that is less than sqrt(number).
    
    2. git ignore modified.

--- PrimeFactorOfFactorial/src/primefactoroffactorial/PrimeFactorOfFactorial.java
+++ PrimeFactorOfFactorial/src/primefactoroffactorial/PrimeFactorOfFactorial.java
@@ -5,17 +5,41 @@
  */
 package primefactoroffactorial;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Naima
  */
 public class PrimeFactorOfFactorial {
-
+    
+    boolean IsPrime(int n){
+        if(n <= 3)
+            return n > 1;
+        if(n % 2 == 0 || n % 3 == 0)
+            return false;
+        int root = (int)Math.floor(Math.sqrt(n));
+        for(int i = 5; i < root; i += 6){
+            if(n % i == 0 || n % (i+2) == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    int Input(){
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        PrimeFactorOfFactorial pf = new PrimeFactorOfFactorial();
+        System.out.println("Enter a number between 1-100 to see if it's prime or not");
+        int n = pf.Input();
+        String str = pf.IsPrime(n)? "Prime" : "Not Prime";
+        System.out.println(str);
     }
     
 }
